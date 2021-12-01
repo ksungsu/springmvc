04ba12044fd71bf86bdf4058ceec4f5a9dbcfa67
ksungsu <ksungsu@naver.com>
2021-12-01 16:07:59 +0900

    Study about SpringMVC

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,25 @@
+package hello.springmvc.basic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogTestController {
+
+    private final Logger logerr = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest(){
+        String name = "Spring";
+
+        logerr.trace("trace log={}", name);
+        logerr.debug("debug log={}", name);
+        logerr.info("info log={}", name);
+        logerr.warn("warn log={}", name);
+        logerr.error("error log={}", name);
+
+        return "OK";
+    }
+}
